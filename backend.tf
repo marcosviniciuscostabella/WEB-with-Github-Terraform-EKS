6e2aaeb82a17740440b4ef40c5e53bf9f10a50d2
Marcos <marcosviniciuscostabella>
2025-06-04 19:12:43 +0200

    nueva version 1.3

--- backend.tf
+++ backend.tf
@@ -1,10 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraformeksgithubbackup" # <-- CAMBIA ESTO con el nombre de tu bucket
-    key    = "terraform/state/eks.tfstate"
-    region = "us-east-1"
-
+    bucket         = "mi-terraform-state"
+    key            = "state/terraform.tfstate"
+    region         = "eu-west-1"
     dynamodb_table = "terraform-lock"
-    encrypt        = true
   }
 }
