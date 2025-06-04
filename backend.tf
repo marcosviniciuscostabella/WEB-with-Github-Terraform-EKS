bbb2f59a90fd4e09f154d6d0c3d2699905780cee
Marcos <marcosviniciuscostabella>
2025-06-04 11:35:40 +0200

    Remove modules/vpc (using official module)

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraformeksgithubbackup"    # <-- CAMBIA ESTO con el nombre de tu bucket
+    key            = "terraform/state/eks.tfstate"
+    region         = "us-east-1"
+
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
